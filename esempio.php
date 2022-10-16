04ff0f9dfc66dbf490b3fb22eff0bb2529ea31e0
Ismaele Boussim <sismaele250@gmail.com>
2022-10-16 16:27:43 +0200

    first commit

---
+++ esempio.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Monphy</title>
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+  <body>
+    <p>
+<?php
+
+echo 'Ciao ' . $_POST['nome'] . ' ' . $_POST['cognome'] . ', grazie per esserti registrato.';
+
+?>
+    </p>
+  </body>
+</html>
